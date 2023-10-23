26e71efa1505a4228e1ebb2c12b11bbddb8cd791
Jörg Oertel <joergoertel@proton.me>
2023-10-23 09:42:15 +0200

    refactored

--- src/main/java/dk/lyngby/kata3/Password.java
+++ src/main/java/dk/lyngby/kata3/Password.java
@@ -2,19 +2,4 @@ package dk.lyngby.kata3;
 
 public class Password {
 
-    private static final String PASSWORD_LENGTH_ERROR = "Password must be at least 8 characters long";
-    private static final String PASSWORD_DIGIT_ERROR = "The password must contain at least 2 digits";
-
-
-    public String passwordValidator(String input) {
-        return "";
-    }
-
-    public boolean validateLength(String input) {
-        return input.length() >= 8;
-    }
-
-    public boolean validateHasTwoDigits(String input) {
-        return input.matches(".*\\d{2}.*");
-    }
 }
